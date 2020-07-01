e6e088a3e928ee3cc0be686d1bf7913247b0911b
Matt Von Lintel <mvonlintel@hotmail.com>
2020-07-01 14:14:04 -0600

    No release notes for this build

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "intrinio-sdk"
-VERSION = "5.6.0"
+VERSION = "5.6.1"
 # To install the library, run the following
 #
 # python setup.py install
